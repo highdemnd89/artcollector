d2aaf6c2ac41d895121c8e2fc3373ea41f0a27f1
Jonathan Mitchell <jonathanmitchell@Jonathans-MacBook-Pro.local>
2022-06-02 20:28:08 -0500

    message

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,11 @@ export { default as Loading } from './Loading';
 export { default as Preview } from './Preview';
 export { default as Search } from './Search';
 export { default as Title } from './Title';
+
+/*
+* 1st we git status --> get what files have been changed
+* 2nd link the github remote --> git remote add nameOfRemote url 
+* 3rd git add --> get these changes ready to commit 
+* 4th git commit -m "What ever message" --> git commit 
+* 5th git push
+*/
