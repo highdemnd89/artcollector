be830f59341fb17ed056283b655a4b225231c20c
Jonathan Mitchell <jonathanmitchell@Jonathans-MacBook-Pro.local>
2022-06-11 09:39:23 -0500

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -9,7 +9,8 @@ export { default as Title } from './Title';
 /*
 * 1st we git status --> get what files have been changed
 * 2nd link the github remote --> git remote add nameOfRemote url 
-* 3rd git add --> get these changes ready to commit 
+* 3rd git add . --> get these changes ready to commit 
 * 4th git commit -m "What ever message" --> git commit 
-* 5th git push
+* 5th git push remoteName branchName
+   - onlineArtCollector master 
 */
